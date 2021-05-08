86450aed1a42fcd3d29905fc42bbdc35f9b480b9
Tushar <tsbauskar0812@gmail.com>
2021-05-08 20:20:02 +0530

    Minor changes

--- gisproject/weighted_means/urls.py
+++ gisproject/weighted_means/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import map_page_2001, map_page_2011, centroids2011, centroids2001, map_page
 
 urlpatterns = [
-    path('map/', map_page, name='map-page'),
+    path('', map_page, name='map-page'),
     path('map/2001', map_page_2001, name='map-page_2001'),
     path('map/2011', map_page_2011, name='map-page_2011'),
     path('2001', centroids2001, name='centroids-2001'),
